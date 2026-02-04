120f4388e3f60c1e7fbdfbe20d6b49aa03ac3d25
Alexnor11 <advance114@gmail.com>
2026-02-04 12:39:18 +0300

    Game Session Manager

---
+++ Assets/_Scripts/Manager/GameSessionManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSessionManager : MonoBehaviour
+{
+	[Tooltip("Оставшиеся жизни игрока")]
+	private int _playerLives = 3;
+	[SerializeField, Tooltip("Место возраждение игрока")]
+	private Transform _respawnLocation;
+	static public GameSessionManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+	public void OnPlayerDeath(GameObject player)
+	{
+		if (_playerLives <= 0)
+		{
+			GameObject.Destroy(player.gameObject);
+			Debug.Log("Game over!");
+		}
+		else
+		{
+			_playerLives--;
+			HealthManager playerHealth = player.GetComponent<HealthManager>();
+			if (playerHealth)
+			{
+				playerHealth.Reset();
+			}
+			if(_respawnLocation)
+			{
+				player.transform.position = _respawnLocation.position;
+			}
+			Debug.Log("Player lives remaining: " + _playerLives);
+		}
+	}
+}
--- Assets/_Scripts/Manager/HealthManager.cs
+++ Assets/_Scripts/Manager/HealthManager.cs
@@ -15,51 +15,7 @@ public class HealthManager : MonoBehaviour
 	[SerializeField, Tooltip("Мертв ли объект")]
 	private bool _isDead = false;
 
-	public float AdjustCurHealth(float change)
-	{
-		if(_invincibilityFramesCur > 0)
-		{
-			return _HealthCur;
-		}
-		_HealthCur += change;
-
-		if(_HealthCur <= 0)
-		{
-			OnDeath();
-		}
-		else if(_HealthCur >= _healthMax)
-		{
-			_HealthCur = _healthMax;			
-		}
-
-		if(change < 0 && _invincibilityFramesMax > 0)
-		{
-			_invincibilityFramesCur = _invincibilityFramesMax;
-		}
-		return _HealthCur;
-	}
-
-	void OnDeath()
-	{
-		if(_HealthCur > 0)
-		{
-            Debug.Log(gameObject.name + " считать мертвым до обнуления здоровья.");
-        }
-		_isDead = true;
-	}
-
-	public bool IsDead()
-	{
-		return _isDead;
-	}
-
-    public void Reset()
-    {
-        _isDead = false;
-		_HealthCur = _healthMax;
-		_invincibilityFramesCur = 0;
-    }
-
+	
     private void Update()
     {
         if(_invincibilityFramesCur > 0)
@@ -70,24 +26,38 @@ public class HealthManager : MonoBehaviour
 				_invincibilityFramesCur = 0;
 			}
 		}
-		if(IsDead())
-			GameObject.Destroy(gameObject);
 
         // обработка видимости объекта
         if (GetComponent<MeshRenderer>())
-		{
-			if (_invincibilityFramesCur > 0)
-			{
-				if (GetComponent<MeshRenderer>().enabled == true)
-				{
-					GetComponent<MeshRenderer>().enabled = false;
-				}
+        {
+            if (_invincibilityFramesCur > 0)
+            {
+                if (GetComponent<MeshRenderer>().enabled == true)
+                {
+                    GetComponent<MeshRenderer>().enabled = false;
+                }
                 else
                 {
                     GetComponent<MeshRenderer>().enabled = true;
                 }
+            }else
+            {
+                GetComponent<MeshRenderer>().enabled = true;
             }
+        }
+        //if(IsDead())
+        //	GameObject.Destroy(gameObject);
+
+        if (_isDead)
+		{
+			if (GetComponent<PlayerController>())
+			{
+				GameSessionManager.Instance.OnPlayerDeath(gameObject);
+			}
+			else
+				GameObject.Destroy(gameObject);
 		}
+        
 		// переключение дрожания камеры для игрока
 		if (GetComponent<PlayerController>())
 		{
@@ -97,5 +67,49 @@ public class HealthManager : MonoBehaviour
 				camShake.enabled = (bool)(_invincibilityFramesCur > 0);
 			}
 		}
+    }
+    public float AdjustCurHealth(float change)
+    {
+        if (_invincibilityFramesCur > 0)
+        {
+            return _HealthCur;
+        }
+        _HealthCur += change;
+
+        if (_HealthCur <= 0)
+        {
+            OnDeath();
+        }
+        else if (_HealthCur >= _healthMax)
+        {
+            _HealthCur = _healthMax;
+        }
+
+        if (change < 0 && _invincibilityFramesMax > 0)
+        {
+            _invincibilityFramesCur = _invincibilityFramesMax;
+        }
+        return _HealthCur;
+    }
+
+    void OnDeath()
+    {
+        if (_HealthCur > 0)
+        {
+            Debug.Log(gameObject.name + " считать мертвым до обнуления здоровья.");
+        }
+        _isDead = true;
+    }
+
+    public bool IsDead()
+    {
+        return _isDead;
+    }
+
+    public void Reset()
+    {
+        _isDead = false;
+        _HealthCur = _healthMax;
+        _invincibilityFramesCur = 0;
     }
 }

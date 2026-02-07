f9005fb8b710a7522bf35a4c8abd13eab6d503b3
Alexnor11 <advance114@gmail.com>
2026-02-07 11:04:03 +0300

    Controlling the camera

---
+++ Assets/_Scripts/FollowCam.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+	[SerializeField, Tooltip("Объект для следования")]
+	private GameObject _camTarget;
+
+	[SerializeField, Tooltip("Смещение цели")]
+	private Vector3 _targetOffset;
+	
+	[SerializeField, Tooltip("Высота от земли")] 
+	private float _camHeigth = 9;
+	
+	[SerializeField, Tooltip("Расстояние до цели")] 
+	float _camDistance = -16;
+
+    private void Update()
+    {
+        if(!_camTarget)
+			return;
+
+		Vector3 targetPos = _camTarget.transform.position;
+		targetPos += _targetOffset;
+		targetPos.y += _camHeigth;
+		targetPos.z += _camDistance;
+		// перемещение камеры к целевой позиции
+		Vector3 camPos = transform.position;
+		transform.position = Vector3.Lerp(camPos, targetPos, Time.deltaTime * 5.0f);
+
+    }
+}

1c19c611d4638f93bf01fb38e0f33b3644fcc906
Alexnor11 <advance114@gmail.com>
2026-02-11 12:49:12 +0300

    Endless ammunition

---
+++ Assets/_Scripts/SelfDestructTimer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestructTimer : MonoBehaviour
+{
+	[SerializeField]
+	float _countdownTimer = 1.5f;
+
+    private void Update()
+    {
+        _countdownTimer -= Time.deltaTime;
+        if(_countdownTimer <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}

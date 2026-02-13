bc062cc2b9081336b468c6adcfb255c7e59c7cf2
Alexnor11 <advance114@gmail.com>
2026-02-13 17:05:18 +0300

    Physics Programming

---
+++ Assets/_Scripts/Physics/PhysicsForceZone.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PhysicsForceZone : MonoBehaviour
+{
+	[SerializeField] float _forceToApply = 1;
+
+    private void Awake()
+    {
+        CapsuleCollider c = GetComponent<CapsuleCollider>();
+
+        if (c)
+        {
+            c.isTrigger = true;
+        }
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        if (rb)
+        {
+            rb.isKinematic = true;
+            rb.useGravity = false;
+        }        
+    }
+
+    private void OnTriggerStay(Collider collider)
+    {
+        GameObject hitObj = collider.gameObject;
+        if(hitObj != null)
+        {
+            Rigidbody rb = hitObj.GetComponent<Rigidbody>();
+            Vector3 dir = transform.up;
+            rb.AddForce(dir * _forceToApply);
+        }
+    }
+}

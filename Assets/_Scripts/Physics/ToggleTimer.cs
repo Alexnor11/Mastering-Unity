eda6163c702c8bb2eca8b49fde4e82db2b2094c6
Alexnor11 <advance114@gmail.com>
2026-02-14 16:05:16 +0300

    The wind area strikes

---
+++ Assets/_Scripts/Physics/ToggleTimer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleTimer : MonoBehaviour
+{
+	[SerializeField] float _curTimer = 0;
+	[SerializeField] float _timerGoal = 3;
+	[SerializeField] List<GameObject> _toggleObj;
+
+    private void Update()
+    {
+        if( _toggleObj == null)
+        {
+            return;
+        }
+        _curTimer += Time.deltaTime;
+        if(_curTimer > _timerGoal )
+        {
+            _curTimer = 0;
+            // перебор объектов и их включение/выключение
+            for(int i = 0;  i < _toggleObj.Count; i++)
+            {
+                bool newVal = !_toggleObj[i].activeSelf;
+                _toggleObj[i].SetActive(newVal);
+            }
+        }
+    }
+}

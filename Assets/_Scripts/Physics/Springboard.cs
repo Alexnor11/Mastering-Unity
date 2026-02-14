6c1f6975168299e673528b70968b952a07d451f9
Alexnor11 <advance114@gmail.com>
2026-02-14 15:36:07 +0300

    springboards

---
+++ Assets/_Scripts/Physics/Springboard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Springboard : MonoBehaviour
+{
+	[SerializeField] float _upwardsForce = 2000f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject hitObj = collision.gameObject;
+        if (hitObj != null)
+        {
+            Rigidbody rb = hitObj.GetComponent<Rigidbody>();
+            rb?.AddForce(0, _upwardsForce, 0);
+        }
+    }
+}
